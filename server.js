38665a0f1a925775128d7d101870f8924b8d56a6
Leonid <scorpion23997@gmail.com>
2021-03-19 13:00:53 +0200

    test something else

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-var port = process.env.PORT || 8080
-app.listen(port)
-console.log('server started ' + port)
+const express = require('express')
+const compression = require('compression')
+const history = require('connect-history-api-fallback')
+
+const staticMiddleWare = express.static('dist')
+
+const app = express()
+app.use(compression())
+app.use(staticMiddleWare)
+app.use(history())
+app.use(staticMiddleWare)
+
+app.use(express.static('dist'))
+app.get('*.js', function (req, res, next) {
+    req.url = req.url + '.gz'
+    res.set('Content-Encoding', 'gzip')
+    next()
+})
+
+app.get('*.js', function (req, res, next) {
+    req.url = req.url + '.gz'
+    res.set('Content-Encoding', 'gzip')
+    next()
+})
+
+app.set('port', (process.env.PORT || 3333))
+app.listen(app.get('port'), () =>
+    console.log('Server @', app.get('port')))
