70ac70c22759d287c7f202d1bc424ea33d0df7ca
Leonid <scorpion23997@gmail.com>
2021-03-19 12:42:17 +0200

    test routing 4

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var path = require('path')
-const connectHistory = require('connect-history-api-fallback')
+const history = require('connect-history-api-fallback')
 var serveStatic = require('serve-static')
 app = express()
-app.use('/', serveStatic(path.join(__dirname, '/dist')), connectHistory())
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}))
 var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
